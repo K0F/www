17900d3c1cc37eca1c301b0f48fd1c1ff05f709c
Krystof Pesek <christoffon@gmail.com>
2011-09-04 00:38:12 +0200

    Major cleanup, software tools added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+"http://www.w3.org/TR/html4/loose.dtd">
 
 <?
 $num_of_commits = 7;
@@ -22,211 +22,215 @@ $num_of_commits = 7;
 
 <table align="center" width="800" cellpadding="5">
 
-    <tbody>
+<thead>
 
-    <!-- HEAD --!>
-    <tr>
-    <th valign="top" colspan="2" align="right" ><h1>lab19.ath.cx</h1></th>
-    </tr>
+<!-- HEAD --!>
+<tr>
+<th valign="top" colspan="2" align="right" ><h1>lab19.ath.cx</h1></th>
+</tr>
 
+</thead>
 
-    <!-- CONTENT --!>
-    <tr>
+<tbody>
 
-    <!-- LEFT ROW --!>
-    <td valign="top" width="50%">
+<!-- CONTENT --!>
+<tr>
 
-    
-    <!-- //////////////////////////// --!>
-    <div class="cell">
-        <h1>free culture</h1>
-
-        <p class="quote">
-            "A constellation of interests is now seeking to increase its ownership and control of creativity. We are told that these interests require new laws and rights that will allow them to control concepts and ideas and protect them from exploitation. They say that this will enrich our lives, create new products and safeguard the possibility of future prosperity. But this is a disaster for creativity, whose health depends on an ongoing, free and open conversation between ideas from the past and the present.
-        </p>
-
-        <p class="quote">
-            In response, we wish to defend the idea of a creative field of concepts and ideas that are free from ownership."
-        </p>
-
-        <p class="sign">
-            <i>-- David Berry, Giles Moss</i> <br/>
-            the whole text is available @ <a href="http://www.libresociety.org/">libresociety.org</a>
-
-        </p>
-    </div>
+<!-- LEFT ROW --!>
+<td valign="top" width="50%">
 
 
-     <!-- //////////////////////////// --!>
-    <div class="cell">
-        <h1>kof's manifesto</h1>
+<!-- //////////////////////////// --!>
+<div class="cell">
+<h1>free culture</h1>
 
-        <p>
-        If there is any source code of my work, there is no owner including myself. Binary outputs can be either sold or collected, whatever. But sources are available to anyone. It is completely public domain and it will remain.
-        </p>
+<p class="quote">
+"A constellation of interests is now seeking to increase its ownership and control of creativity. We are told that these interests require new laws and rights that will allow them to control concepts and ideas and protect them from exploitation. They say that this will enrich our lives, create new products and safeguard the possibility of future prosperity. But this is a disaster for creativity, whose health depends on an ongoing, free and open conversation between ideas from the past and the present.
+</p>
 
+<p class="quote">
+In response, we wish to defend the idea of a creative field of concepts and ideas that are free from ownership."
+</p>
 
-        <p class="sign">
-            all of them are available @ <a href="https://github.com/K0F">github.com</a>
-        </p>
-    </div>
+<p class="sign">
+<i>-- David Berry, Giles Moss</i> <br/>
+the whole text is available @ <a href="http://www.libresociety.org/">libresociety.org</a>
 
-  <!-- //////////////////////////// --!>
-    <div class="cell">
-        <h1>fair use</h1>
-
-        <p>
-        Please feel free to share / copy / use / modify any of my works available online. If you would like to screen or modify some of my work and show publicly, please quote me -- either <i>Kof</i> or <i>Krystof Pesek</i> is perfectly OK.
-        </p>
+</p>
+</div>
 
 
-        <p class="sign">
-            kof@<a href="http://openprocessing.org/portal/?userID=3942">openprocessing.org</a><br />
-            kof@<a href="http://vimeo.com/kof/videos/">vimeo.com</a>
-        </p>
-    </div>
+<!-- //////////////////////////// --!>
+<div class="cell">
+<h1>kof's manifesto</h1>
 
-    <div class="cell">
-	<h1>at absurdum</h1>
-
-
-	<p>
-	There is a source of this page hosted on github, quite weird :)
-	</p>
+<p>
+If there is any source code of my work, there is no owner including myself. Binary outputs can be either sold or collected, whatever. But sources are available to anyone. It is completely public domain and it will remain.
+</p>
 
 
-	<?
-        $contents = file_get_contents('http://github.com/api/v2/json/commits/list/K0F/www/master?page=1');
-        
-        //$obj = array();
-        $commits = json_decode($contents,true);
+<p class="sign">
+all of them are available @ <a href="https://github.com/K0F">github.com</a>
+</p>
+</div>
 
-        
-        
-        $all = $commits['commits'];
-        
+<!-- //////////////////////////// --!>
+<div class="cell">
+<h1>fair use</h1>
 
-
-        for($i = 0 ; $i < 5 ;$i++){
-        
-        $commit = $all[$i];
-
-        //var_dump($commit);
-
-        $message = $commit["message"];
-        $url = $commit["url"];
-        $id = $commit["id"];
-        $time = $commit["committed_date"];
-      
-
-        /*
-        $fulltree = $commit["tree"];
-        $tree = file_get_contents('http://github.com/api/v2/json/blob/all/K0F/2011/'.$fulltree);
-        $parsedtree = json_decode($tree,true);
-
-        for($ii = 0;$ii < count($tree); $ii++){
-            echo $tree[$ii];
-
-        }
-        */    
+<p>
+Please feel free to share / copy / use / modify any of my works available online. If you would like to screen or modify some of my work and show publicly, please quote me -- either <i>Kof</i> or <i>Krystof Pesek</i> is perfectly OK.
+</p>
 
 
-        $time = preg_replace('/T.*/', '',$time);
-        //$time = preg_replace('/-07:00/', '',$time);
-        echo '<p class="quote">';
-        echo '<a href="http://github.com'.$url.'">'.$time.'</a>';
-        echo '   :: '.$message;
-        echo '</p>';
+<p class="sign">
+kof@<a href="http://openprocessing.org/portal/?userID=3942">openprocessing.org</a><br />
+kof@<a href="http://vimeo.com/kof/videos/">vimeo.com</a>
+</p>
+</div>
 
-        }
-        
-        ?>
- 
-
-    </div>
-
-    <div class="cell">
-
-        <h1>recent coding activity 2011</h1>
-        <?
-        $contents = file_get_contents('http://github.com/api/v2/json/commits/list/K0F/2011/master?page=1');
-        
-        //$obj = array();
-        $commits = json_decode($contents,true);
-
-        
-        
-        $all = $commits['commits'];
-        
+<div class="cell">
+<h1>at absurdum</h1>
 
 
-        for($i = 0 ; $i < $num_of_commits ;$i++){
-        
-        $commit = $all[$i];
-
-        //var_dump($commit);
-
-        $message = $commit["message"];
-        $url = $commit["url"];
-        $id = $commit["id"];
-        $time = $commit["committed_date"];
-      
-
-        /*
-        $fulltree = $commit["tree"];
-        $tree = file_get_contents('http://github.com/api/v2/json/blob/all/K0F/2011/'.$fulltree);
-        $parsedtree = json_decode($tree,true);
-
-        for($ii = 0;$ii < count($tree); $ii++){
-            echo $tree[$ii];
-
-        }
-        */    
+<p>
+There is a source of this page hosted on github, quite weird :)
+</p>
 
 
-        $time = preg_replace('/T.*/', '',$time);
-        //$time = preg_replace('/-07:00/', '',$time);
-        echo '<p class="quote">';
-        echo '<a href="http://github.com'.$url.'">'.$time.'</a>';
-        echo '   :: '.$message;
-        echo '</p>';
+<?
+$contents = file_get_contents('http://github.com/api/v2/json/commits/list/K0F/www/master?page=1');
 
-        }
-        
-        ?>
-    </div>
-
-
-    <div class="cell">
-        <h1>contact</h1>
-        
-        <p>
-        For any questions etc. please do not hesitate to contact me:
-        </p>
-
-        <p> 
-        <div class="mail"></div>
-        </p>
-
-        <p>
-
-        ...
-        
-        </p>
-
-
-    </div>
+//$obj = array();
+$commits = json_decode($contents,true);
 
 
 
+$all = $commits['commits'];
 
 
 
-    <!-- RIGHT ROW --!>
-    <td valign="top" width="50%">
+for($i = 0 ; $i < 5 ;$i++){
 
-        <div class="cell">
-            <h1>16:9 of something</h1>
+	$commit = $all[$i];
+
+	//var_dump($commit);
+
+	$message = $commit["message"];
+	$url = $commit["url"];
+	$id = $commit["id"];
+	$time = $commit["committed_date"];
+
+
+	/*
+	   $fulltree = $commit["tree"];
+	   $tree = file_get_contents('http://github.com/api/v2/json/blob/all/K0F/2011/'.$fulltree);
+	   $parsedtree = json_decode($tree,true);
+
+	   for($ii = 0;$ii < count($tree); $ii++){
+	   echo $tree[$ii];
+
+	   }
+	 */    
+
+
+	$time = preg_replace('/T.*/', '',$time);
+	//$time = preg_replace('/-07:00/', '',$time);
+	echo '<p class="quote">';
+	echo '<a href="http://github.com'.$url.'">'.$time.'</a>';
+	echo '   :: '.$message;
+	echo '</p>';
+
+}
+
+?>
+
+
+</div>
+
+<div class="cell">
+
+<h1>recent coding activity 2011</h1>
+<?
+$contents = file_get_contents('http://github.com/api/v2/json/commits/list/K0F/2011/master?page=1');
+
+//$obj = array();
+$commits = json_decode($contents,true);
+
+
+
+$all = $commits['commits'];
+
+
+
+for($i = 0 ; $i < $num_of_commits ;$i++){
+
+	$commit = $all[$i];
+
+	//var_dump($commit);
+
+	$message = $commit["message"];
+	$url = $commit["url"];
+	$id = $commit["id"];
+	$time = $commit["committed_date"];
+
+
+	/*
+	   $fulltree = $commit["tree"];
+	   $tree = file_get_contents('http://github.com/api/v2/json/blob/all/K0F/2011/'.$fulltree);
+	   $parsedtree = json_decode($tree,true);
+
+	   for($ii = 0;$ii < count($tree); $ii++){
+	   echo $tree[$ii];
+
+	   }
+	 */    
+
+
+	$time = preg_replace('/T.*/', '',$time);
+	//$time = preg_replace('/-07:00/', '',$time);
+	echo '<p class="quote">';
+	echo '<a href="http://github.com'.$url.'">'.$time.'</a>';
+	echo '   :: '.$message;
+	echo '</p>';
+
+}
+
+?>
+</div>
+
+
+<div class="cell">
+<h1>contact</h1>
+
+<p>
+For any questions etc. please do not hesitate to contact me:
+</p>
+
+<p> 
+<div class="mail"></div>
+</p>
+
+<p>
+Please do not ask me how to get particular codes working, there are plenty of them and I am trying to write some simple documentation if possible, it is hard work for me. It is my bad long-term memory, I am sorry, I don't remember, I don't know.
+</p>
+</div>
+
+
+
+<div class="cell">
+
+<p class="sign">
+build with great tools: <a href="http://www.vim.org/">vim</a> / <a href="http://www.php.net/">php</a> / <a href="http://httpd.apache.org/">apache</a> / <a href="http://git-scm.com/">git</a> / <a href="http://code.google.com/p/grafx2/">grafx</a> and mostly <a href="http://processing.org/">processing</a>
+</p>
+</div>
+
+
+<!-- RIGHT ROW --!>
+<td valign="top" width="50%">
+
+<div class="cell">
+<h1>16:9 of something</h1>
 
 <iframe src="http://player.vimeo.com/video/27203486?title=0&amp;byline=0&amp;portrait=0&amp;color=f2583e&amp;autoplay=1&amp;loop=1" width="390" height="219" frameborder="0"></iframe>
 
@@ -237,7 +241,7 @@ $num_of_commits = 7;
 Remake of: <a href="http://openprocessing.org/visuals/?visualID=30364">sketch</a> by Stephen Monslow</p>
 </p>
 </div>
-   
+
 
 
 <div class="cell">
@@ -264,15 +268,15 @@ Remake of: <a href="http://openprocessing.org/visuals/?visualID=30364">sketch</a
 <iframe src="http://player.vimeo.com/video/14564533?title=0&amp;byline=0&amp;portrait=0&amp;color=f2583e&amp;autoplay=0&amp;loop=1" width="389" height="219" frameborder="0"></iframe><p>Ode to change. A change as a necessity to perceive. The Change is a word which is trying to describe / capture? (to stop in the way) some current happening.</p>
 </div>
 
-    </td>
-    
-    <!-- END CONTENT --!>
-    </tr>
-    
 
-    </tbody>
 
-    </table>
+<!-- END CONTENT --!>
+
+
+</td>
+</tr>
+</tbody>
+</table>
 
 
 </div>
